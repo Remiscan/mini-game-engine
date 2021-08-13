ae50bf527f306ed14c656f3abbbc4ef948ab64b4
Rémi Scandella <remi.scandella@gmail.com>
2021-08-13 23:36:34 +0200

    start, update functions & button detection

---
+++ game-engine.js
@@ -0,0 +1,112 @@
+/**
+ * Game action, with a name and a list of associated controls.
+ * @typedef {Object} Action
+ * @property {string} name - Name of the action (example: 'jump');
+ * @property {string[]} controls - List of controls / button codes associated with that action (example: ['A', 'Space']);
+ */
+
+export default class Game {
+  /**
+   * Builds a new game.
+   * @param {function} start - The function executed on game launch.
+   * @param {function} update - The function executed on each frame.
+   * @param {object} options
+   * @param {Element} options.canvas - The canvas on which all game objects will be drawn.Node
+   * @param {Element} options.html - The container of all HTML elements that will be displayed over the canvas, for example for accessible text or buttons.
+   * @param {object} options.state - The initial state data of the game.
+   * @param {Action} options.actions - The list of actions and their associated controls.
+   * @param {number} options.frameRate - The number of frames that will be computed per second.
+   */
+  constructor(start, update, {
+    canvas = document.querySelector('canvas'),
+    html = document.querySelector('accessible-elements'),
+    state = {},
+    actions = {},
+    frameRate = 60
+  } = {}) {
+    this.canvas = canvas;
+    this.html = html;
+
+    // State of the game, used to compute what to display on the next frame
+    this.state = state;
+    this.state.controls = {};
+    this.state.actions = actions;
+
+    this.frameRate = frameRate;
+    this.paused = false;
+    
+    this.start = start.bind(this); // Function executed on game launch
+    this.update = update.bind(this); // Function executed every frame
+  }
+
+
+  /** Starts the game. */
+  play() {
+    this.initControls();
+    this.start();
+
+    const pausable = function* () {
+      let i = 0;
+      while (true) {
+        this.update();
+        yield i;
+        i++;
+      }
+    };
+    const pausableUpdate = pausable.bind(this)();
+    setInterval(() => {
+      if (!this.paused) pausableUpdate.next();
+    }, 1000 / this.frameRate);
+  }
+
+
+  /** Pauses the game by pausing the update function. */
+  pause() { this.paused = true; }
+
+  /** Unpauses the game by unpausing the update function. */
+  unpause() { this.paused = false; }
+
+
+  /** Initializes detection of button presses. */
+  initControls() {
+    // Detect keydown events and update the list of controls.
+    document.addEventListener('keydown', event => {
+      const buttonID = event.code || event.key;
+      let button = this.state.controls[buttonID];
+
+      const actions = this.state.actions.filter(a => a.controls.includes(buttonID));
+      actions.map(a => a.active = true);
+
+      if (typeof button === 'undefined') {
+        this.state.controls[buttonID] = {
+          pressed: false,
+          startTime: null,
+          endTime: null
+        };
+      }
+
+      button = button || this.state.controls[buttonID];
+      button.pressed = true;
+      button.startTime = Date.now();
+      button.endTime = null;
+    });
+
+    // Detect keyup events and update the list of controls.
+    document.addEventListener('keyup', event => {
+      const buttonID = event.code || event.key;
+      let button = this.state.controls[buttonID];
+
+      const actions = this.state.actions.filter(a => a.controls.includes(buttonID));
+      actions.map(a => a.active = false);
+
+      if (typeof button === 'undefined') return;
+      button.pressed = false;
+      button.startTime = null;
+      button.endTime = Date.now();
+    });
+  }
+
+  get currentActions() {
+    return this.state.actions.filter(a => a.active).map(a => a.name);
+  }
+}
---
+++ test/game.js
@@ -0,0 +1,60 @@
+import Game, * as GEM from '../game-engine.js';
+
+
+
+let startTime;
+
+const start = function() {
+  startTime = Date.now();
+};
+
+
+const update = function() {
+  const endTime = Date.now();
+  console.log(`New frame after ${endTime - startTime} ms`);
+
+  const currentActions = this?.currentActions;
+  console.log(currentActions);
+
+  if (currentActions.includes('Jump')) { this.state.jumpFrames++; }
+  else {
+    if (this.state.jumpFrames > 0) {
+      console.log(`Jumped for ${this.state.jumpFrames} frames`);
+      this.state.jumpFrames = 0;
+    }
+  }
+
+  startTime = endTime;
+};
+
+
+const actions = [
+  {
+    name: 'Jump',
+    controls: ['Space']
+  }, {
+    name: 'Up',
+    controls: ['ArrowUp', 'KeyW']
+  }, {
+    name: 'Down',
+    controls: ['ArrowDown', 'KeyS']
+  }, {
+    name: 'Left',
+    controls: ['ArrowLeft', 'KeyA']
+  }, {
+    name: 'Right',
+    controls: ['ArrowRight', 'KeyD']
+  }
+];
+
+
+const state = {
+  jumpFrames: 0
+};
+
+
+const game = new Game(start, update, { frameRate: 4, actions, state });
+game.play();
+
+
+window.addEventListener('click', () => game.paused = !game.paused);
