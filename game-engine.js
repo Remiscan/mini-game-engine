7671c754f9b9c97791640cb3a15f6076eb97c264
Rémi Scandella <remi.scandella@gmail.com>
2021-08-14 02:29:32 +0200

    Initial work on game Levels and sounds

--- game-engine.js
+++ game-engine.js
@@ -5,6 +5,11 @@
  * @property {string[]} controls - List of controls / button codes associated with that action (example: ['A', 'Space']);
  */
 
+
+
+/**
+ * @class Game object.
+ */
 export default class Game {
   /**
    * Builds a new game.
@@ -15,6 +20,7 @@ export default class Game {
    * @param {Element} options.html - The container of all HTML elements that will be displayed over the canvas, for example for accessible text or buttons.
    * @param {object} options.state - The initial state data of the game.
    * @param {Action} options.actions - The list of actions and their associated controls.
+   * @param {Level} options.levels - The list of game levels.
    * @param {number} options.frameRate - The number of frames that will be computed per second.
    */
   constructor(start, update, {
@@ -22,6 +28,7 @@ export default class Game {
     html = document.querySelector('accessible-elements'),
     state = {},
     actions = {},
+    levels = [],
     frameRate = 60
   } = {}) {
     this.canvas = canvas;
@@ -32,7 +39,11 @@ export default class Game {
     this.state.actions = actions;
 
     this.frameRate = frameRate;
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     this.paused = false;
+    this.mute = false;
+    this.levels = levels;
+    this.currentLevel = null;
     
     this.start = start.bind(this); // Function executed on game launch
     this.update = update.bind(this); // Function executed every frame
@@ -56,6 +67,37 @@ export default class Game {
     setInterval(() => {
       if (!this.paused) pausableUpdate.next();
     }, 1000 / this.frameRate);
+  }
+
+
+  /**
+   * Loads a level, its sprites and its sounds.
+   * @param {string} id - Identifier of the level.
+   */
+  async loadLevel(id) {
+    this.currentLevel = this.levels.find(level => level.id === id);
+
+    // Load sounds.
+    let responses = await Promise.all(this.currentLevel.sounds.map(sound => fetch(`./assets/${sound}.mp3`)));
+    responses = await Promise.all(responses.map(r => r.arrayBuffer()));
+    responses = await Promise.all(responses.map(r => audioCtx.decodeAudioData(r)));
+    this.currentLevel.sounds = this.currentLevel.sounds.map((id, k) => { return { id: id, sound: responses[k] } });
+  }
+
+
+  /**
+   * Plays a sound.
+   * @param {string} id - Identifier of the sound.
+   */
+  playSound(id) {
+    if (this.mute) return;
+
+    const sound = this.audioCtx.createBufferSource();
+    const k = this.currentLevel.sounds.findIndex(s => s.id == id);
+    if (k == -1) throw `Sound ${id} doesn't exist`;
+    sound.buffer = this.currentLevel.sounds[k].sound;
+    sound.connect(this.audioCtx.destination);
+    sound.start();
   }
 
 
@@ -83,7 +125,27 @@ export default class Game {
     });
   }
 
+
+  /** @returns {Array} The list of currently active actions. */
   get currentActions() {
     return this.state.actions.filter(a => a.active).map(a => a.name);
+  }
+}
+
+
+
+/**
+ * @class Game level object.
+ * @property {object} sprites - The list of sprites used in the level.
+ * @property {object} sounds - The list of sounds used in the level.
+ * @property {AudioContext|webkitAudioContext} audioCtx - The audio context.
+ */
+export class Level {
+  constructor({
+    sprites = {},
+    sounds = []
+  }) {
+    this.sprites = sprites;
+    this.sounds = sounds.map(name => { return { id: name, sound: null } });
   }
 }
